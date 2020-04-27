cac9574c4c2e2abe64bc4e3d9ed402809642ed6a
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-27 10:48:45 -0600

    Implemented DELETE in dal

--- dal/video-games.js
+++ dal/video-games.js
@@ -83,17 +83,45 @@ const addGame = (videoGame) => {
     return myPromise;
 };
 
-//PUT function
+//UPDATE: PUT function
 const updateGame = (id, videoGame) => {};
 
-//PATCH function
+//UPDATE: PATCH function
 const updateGameValues = (id, videoGame) => {};
 
 //DELETE function
-const deleteGame = (id) => {};
+const deleteGame = (id) => {
+    const myPromise = new Promise((reject) => {
+        if(err){
+            reject(err);
+        }else{
+            console.log("Connected to DB server for DELETE");
+            const db = client.db(dbName);
+            const collection = db.collection(colName);
+            try{
+                const _id = new ObjectID(id);
+                collection.findOneAndDelete({_id}, function (err, data) {
+                    if(err){
+                        reject(err);
+                    }else{
+                        if(data.lastErrorObject.n > 0) {
+                            resolve(data.value);
+                        }else{
+                            resolve({error: "ID doesn't exist in database"})
+                        }
+                    }
+                })
+            }catch(err){
+                reject({ error: "ID has to be in ObjectID format"})
+            }
+        }
+    });
+    return myPromise;
+};
 
 module.exports = {
     getGames,
     getGameById,
-    addGame
+    addGame,
+    deleteGame
 }
