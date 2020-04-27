64766d74404cad351eaf7a38e537f1908fae3cb2
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-27 10:04:46 -0600

    Implemented READ all in dal

--- dal/video-games.js
+++ dal/video-games.js
@@ -5,8 +5,35 @@ const ObjectID = require('mongodb').ObjectID;
 const url = process.env.ATLAS_CONNECTION;
 const settings = {useUnifiedTopology: true};
 
+//Database and Collection Names
+const dbName = 'game_collection';
+const colName = 'video_games';
+
 //READ functions
-const getGames = () => {};
+const getGames = () => {
+    const myPromise = new Promise((resolve, reject) =>{
+        MongoClient.connnect(url, settings, function(err, client) {
+            if(err){
+                reject(err);
+            }else{
+                console.log("Connected to DB server for READ");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.find({}).toArray(function(err, docs) {
+                    if(err){
+                        reject(err);
+                    } else {
+                        console.log("Found the following Video Games")
+                        console.log(docs);
+                        resolve(docs);
+                        client.close();
+                    }
+                });
+            }
+        });
+    });
+    return myPromise;
+};
 const getGameById = (id) => {};
 const getGameByTitle = (title) => {};
 
