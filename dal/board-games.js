b35c444ab67c05bd386b30423184b1f60b0a07b7
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-15 16:01:26 -0600

    Implemented CRUD functionality to board games data access layer

---
+++ dal/board-games.js
@@ -0,0 +1,223 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+//Connection URL and Database Settings
+const url = process.env.ATLAS_CONNECTION;
+const settings = {useUnifiedTopology: true};
+
+//Database and Collection Names
+const dbName = 'game_collection';
+const colName = 'board_games';
+
+//READ functions
+const getGames = () => {
+    const myPromise = new Promise((resolve, reject) =>{
+        MongoClient.connect(url, settings, function(err, client) {
+            if(err){
+                reject(err);
+            }else{
+                console.log("Connected to DB server for READ");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.find({}).toArray(function(err, docs) {
+                    if(err){
+                        reject(err);
+                    } else {
+                        console.log("Found the following Board Games")
+                        console.log(docs);
+                        resolve(docs);
+                        client.close();
+                    }
+                });
+            }
+        });
+    });
+    return myPromise;
+};
+const getGameById = (id) => {
+    const myPromise = new Promise((resolve, reject) =>{
+        MongoClient.connect(url, settings, async function(err, client) {
+            if(err){
+                reject(err);
+            }else{
+                console.log("Connected to DB server for READ by ID");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                try{
+                    const _id = new ObjectID(id);
+                    const result = await collection.findOne({_id});
+                    if(result){
+                        resolve(result);
+                    }else{
+                        reject({error: "ID not found in database"});
+                    }
+                    client.close();
+                }catch(err){
+                    reject({error: "ID must be in ObjectID format"});
+                }
+            }
+        });
+    });
+    return myPromise;
+};
+
+const getGameByTitle = (title) => {
+    const myPromise = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, async function(err, client) {
+            if(err){
+                reject(err);
+            }else{
+                console.log("Connected to DB server for READ by Title");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                try{
+                    const result = await collection.findOne({title});
+                    if(result){
+                        resolve(result);
+                    }else{
+                        reject({error: "Title not found in database"})
+                    }
+                    client.close();
+                }catch(err){
+                    reject(err);
+                }
+            }
+        });
+    });
+    return myPromise;
+};
+
+//CREATE function
+const addGame = (boardGame) => {
+    const myPromise = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, function(err, client) {
+            if(err){
+                reject(err);
+            }else{
+                console.log("Connected to DB server for CREATE");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.insertOne(boardGame, (err, result) => {
+                    if(err){
+                        reject(err);
+                    } else{
+                        resolve(result.ops[0]);
+                        client.close;
+                    }
+                })
+            }
+        })
+    });
+    return myPromise;
+};
+
+//UPDATE: PUT function
+const updateGame = (id, boardGame) => {
+    const myPromise = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, function(err, client) {
+            if(err){
+                reject(err);
+            }else{
+                console.log('Connected to DB server for for UPDATE: PUT');
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.replaceOne({_id: ObjectID(id)},
+                    boardGame,
+                    {upsert: true},
+                    (err, result) => {
+                        if(err){
+                            reject(err);
+                        }else{
+                            resolve({updated_id: id});
+                            client.close;
+                        }
+                    })
+            }
+        })
+    });
+    return myPromise;
+};
+
+//UPDATE: PATCH function
+const updateGameValues = (id, boardGame) => {
+    const myPromise = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, async function(err, client) {
+            if(err){
+                reject(err);
+            }else{
+                console.log("Connected to DB server for UPDATE: PATCH");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                try{
+                    const _id = new ObjectID(id);
+                    collection.updateOne({_id},
+                        {$set: {...boardGame} },
+                        function (err, data) {
+                            if(err){
+                                reject(err);
+                            }else{
+                                if(data.result.n > 0){
+                                    collection.find({_id}).toArray(
+                                        function(err, docs){
+                                            if(err){
+                                                reject(err);
+                                            }else{
+                                                resolve(docs[0]);
+                                            }
+                                        }
+                                    )
+                                }else{
+                                    resolve({error: "Nothing happened"})
+                                }
+                            }
+                        });
+                }catch(err){
+                    reject({error: "ID has to be in ObjectID format"})
+                }
+            }
+        });
+    });
+    return myPromise;
+};
+
+//DELETE function
+const deleteGame = (id) => {
+    const myPromise = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, async function(err, client) {
+            if(err){
+                reject(err);
+            }else{
+                console.log("Connected to DB server for DELETE");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                try{
+                    const _id = new ObjectID(id);
+                    collection.findOneAndDelete({_id}, function (err, data) {
+                        if(err){
+                            reject(err);
+                        }else{
+                            if(data.lastErrorObject.n > 0) {
+                                resolve(data.value);
+                            }else{
+                                resolve({error: "ID doesn't exist in database"});
+                            }
+                        }
+                    });
+                }catch(err){
+                    reject({ error: "ID has to be in ObjectID format"});
+                }
+            }
+        });
+    });
+    return myPromise;
+};
+
+module.exports = {
+    getGames,
+    getGameById,
+    getGameByTitle,
+    addGame,
+    updateGame,
+    updateGameValues,
+    deleteGame
+}
